9ac5db7f8901d53a180025f73a076801999403a2
Your Name <you@example.com>
2020-04-12 02:49:34 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?types=country&access_token=pk.eyJ1IjoibWFsaW5kdTg4IiwiYSI6ImNrOG5weWdzbDBzbG4zZnFjeDlkbXJ3amEifQ.NPz50CbZzjjX8bUKtu4t0g&limit=1';
+
+    request({url: url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to location services!', undefined)
+        } else if(body.features.length === 0){
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                latitude : body.features[0].center[0], 
+                longitude : body.features[0].center[1], 
+                location : body.features[0].place_name 
+            })
+        }
+    })
+}
+
+module.exports = geoCode
